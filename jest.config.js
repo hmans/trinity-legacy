4840873659a87363e5afa4b339b8d9a91aa8c54c
Hendrik Mans <hendrik@mans.de>
2020-12-24 20:03:59 +0000

    Get tests working

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   verbose: true,
   preset: "ts-jest",
-  roots: ["src", "test"],
+  roots: ["src"],
   testMatch: ["**/__tests__/**/*.+(ts|tsx|js)", "**/?(*.)+(spec|test).+(ts|tsx|js)"],
   testPathIgnorePatterns: ["node_modules"],
   globals: {
