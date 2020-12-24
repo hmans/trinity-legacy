0c988350cc3d626c57fe87c9a0853d6d9dc257fb
Hendrik Mans <hendrik@mans.de>
2020-12-24 21:03:42 +0000

    Documents

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { Reactor } from "./reactor/Reactor"
+export { Engine, Scene, useScene } from "./engine"
+export * from "./engine/hooks"
 export { makeComponent } from "./reactor/makeComponent"
 export { useManagedThreeObject } from "./reactor/useManagedThreeObject"
-export { Engine } from "./engine"
-export { Scene, useScene } from "./engine"
-export * from "./engine/hooks"
-
 export { Reactor }
-// export default Reactor
+
+export default Reactor
