442c9c7b3dd82d39006c43f4181c0558e095228a
Hendrik Mans <hendrik@mans.de>
2020-12-29 14:50:14 +0100

    Restructure repository

--- index.ts
+++ index.ts
@@ -1,25 +1,13 @@
 /* Types */
 export * from "./types"
 
-/* Common */
-export { applyProps, applyEventProps, applyRef } from "./common"
-
 /* Reactor */
-export {
-  Reactor,
-  Primitive,
-  useManagedThreeObject,
-  makeComponent,
-  forwardRefReactor
-} from "./reactor"
+export { Reactor, Primitive } from "./reactor"
 
 /* Engine */
 export { Engine } from "./engine/Engine"
 export { Scene, useScene } from "./engine/Scene"
 export * from "./engine/hooks"
-
-/* Loaders */
-export * from "./loaders"
 
 /* Default export */
 import { Reactor } from "./reactor"
